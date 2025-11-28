e90b842f6407471285e30c4ab1cc40ed749c5c6d
tsravanreddy <sravankumarreddy.talakanti@viasat.com>
2025-11-28 16:20:07 +0530

    Create jh.cs

---
+++ jh.cs
@@ -0,0 +1,27 @@
+namespace Dbn
+{
+    public class Dbm
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public bool IsNameSet()
+        {
+            return !string.IsNullOrEmpty(Name);
+        }
+
+        public string GetGreeting()
+        {
+            return IsNameSet() ? $"Hello, {Name}!" : "Hello!";
+        }
+    }
+}
